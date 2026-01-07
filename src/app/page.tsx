6d14c1ffddc15ee89b42b324078faa33b988da8b
MirosBL45 <jovicmiroslav88@gmail.com>
2026-01-08 00:25:56 +0100

    scss added successfully

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import ForSave from '@/components/ForSave';
+import styles from './page.module.scss';
+
 export default function Home() {
   return (
     <div>
-      <h1>Slamarica</h1>
+      <h1 className={styles.title}>Slamarica</h1>
+      <ForSave />
     </div>
   );
 }
---
+++ src/components/ForSave.tsx
@@ -0,0 +1,10 @@
+import styles from './forsave.module.scss';
+
+export default function ForSave() {
+  return (
+    <div className={styles.blog}>
+      <h1 className={styles.title}>ForSave</h1>
+      <p className={styles.content}>This is the ForSave component.</p>
+    </div>
+  );
+}
