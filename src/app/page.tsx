a66977f093cf380e6dcb57fc3165a3ff9ec370cb
Miroslav Jovic <89506753+MirosBL45@users.noreply.github.com>
2026-01-10 01:45:45 +0100

    feat: add Ant Design setup (#2)
    * feat: add Ant Design setup
    
    * chore: address review comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { Button, Card, Form, InputNumber } from 'antd';
+
 import ForSave from '@/components/ForSave';
 import styles from './page.module.scss';
 
@@ -6,6 +10,19 @@ export default function Home() {
     <div>
       <h1 className={styles.title}>Slamarica</h1>
       <ForSave />
+      <section>
+        <Card style={{ maxWidth: 400, margin: '2rem auto' }}>
+          <Form layout="vertical">
+            <Form.Item label="Plata">
+              <InputNumber style={{ width: '100%' }} />
+            </Form.Item>
+
+            <Button type="primary" block>
+              Potvrdi
+            </Button>
+          </Form>
+        </Card>
+      </section>
     </div>
   );
 }
