4b819a501adaa8b8d57b3be6efcbb81478abc63f
MirosBL45 <jovicmiroslav88@gmail.com>
2026-01-10 01:13:23 +0100

    feat: add Ant Design setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import '@/styles/globals.scss';
 
+import { AntdRegistry } from '@ant-design/nextjs-registry';
+
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <AntdRegistry>{children}</AntdRegistry>
       </body>
     </html>
   );
