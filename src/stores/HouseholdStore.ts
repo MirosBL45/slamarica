9557877dfccf9207808ade2a979ad9df1cf73dc5
Miroslav <tatazamatu@gmail.com>
2026-01-13 14:43:03 +0100

    Implemented MobX state management for Household and Members data. (#4)
    * Implemented MobX state management for Household and Members data.
    
    * interface starting with I

--- src/stores/HouseholdStore.ts
+++ src/stores/HouseholdStore.ts
@@ -1,7 +1,22 @@
 import { makeAutoObservable } from 'mobx';
 
+export interface IHousehold {
+    id: string;
+    name: string;
+}
+
 export class HouseholdStore {
+    household: IHousehold | null = null;
+
     constructor() {
         makeAutoObservable(this);
+    }
+
+    setHousehold(household: IHousehold) {
+        this.household = household;
+    }
+
+    clearHousehold() {
+        this.household = null;
     }
 }
--- src/stores/MembersStore.ts
+++ src/stores/MembersStore.ts
@@ -1,7 +1,26 @@
 import { makeAutoObservable } from 'mobx';
 
+export interface IMember {
+    id: string;
+    name: string;
+}
+
 export class MembersStore {
+    members: IMember[] = [];
+
     constructor() {
         makeAutoObservable(this);
+    }
+
+    addMember(member: IMember) {
+        this.members.push(member);
+    }
+
+    removeMember(memberId: string) {
+        this.members = this.members.filter(m => m.id !== memberId);
+    }
+
+    clearMembers() {
+        this.members = [];
     }
 }
