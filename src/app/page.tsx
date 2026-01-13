0817871ede826bda286fe600572def588ed3fc02
MiroslavDev45 <tatazamatu@gmail.com>
2026-01-13 15:57:47 +0100

    aded p tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,20 @@ export default function Home() {
     <div>
       <h1 className={styles.title}>Slamarica</h1>
       <ForSave />
-      <p>ispod ide test</p>
+      <p>
+        ispod ide test Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Nostrum quaerat odio voluptatibus nobis. Sunt, corrupti rem. Libero
+        facilis vitae rem voluptates velit fugiat earum quam atque totam
+        consequuntur. At, voluptas.
+      </p>
       <TestForMobX />
       <p>iznad ide test</p>
+      <p>
+        ispod ide jos jedan test Lorem ipsum dolor sit amet consectetur,
+        adipisicing elit. Quaerat deleniti quam ab magnam eos, possimus saepe
+        deserunt quae repellat soluta ullam, cum consequatur tempore minima.
+        Reprehenderit magni neque numquam cumque.
+      </p>
       <TestPools />
       <p>iznad je test pool</p>
       <section>
