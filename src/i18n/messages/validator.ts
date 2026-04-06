2f24801c6293dd4731db5490a328f517de0ecb46
Miroslav Jović <89506753+MirosBL45@users.noreply.github.com>
2026-04-06 12:09:30 +0200

    fixed new text (#81)
    * fixed new text
    
    * deleted comment and validator reorder import

--- src/i18n/messages/validator.ts
+++ src/i18n/messages/validator.ts
@@ -1,8 +1,8 @@
 import { Messages } from "../request";
-import sr from "./sr.json";
-import en from "./en.json";
 import de from "./de.json";
+import en from "./en.json";
 import es from "./es.json";
+import sr from "./sr.json";
 
 // Ova promenljiva osigurava da svi jezici prate strukturu sr.json
 const check: Record<string, Messages> = {
